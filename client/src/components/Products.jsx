eadbe62556ce00ab0cbdf35082dd5bce49a19cda
VinayakG311 <96966973+VinayakG311@users.noreply.github.com>
2023-06-27 16:51:13 +0530

    Update Products.jsx

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -20,8 +20,8 @@ const Products = ({ cat, filters, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:3000/api/products?category=${cat}`
-            : "http://localhost:3000/api/products"
+            ? `https://aura-backend-5f6l.onrender.com/api/products?category=${cat}`
+            : "https://aura-backend-5f6l.onrender.com/api/products"
         );
         setProducts(res.data);
       } catch (err) { }
